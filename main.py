8aa07044d9f85053725376d75e5ad6356171c0f4
NazaniGit <paulohmotta20111@gmail.com>
2025-11-03 11:44:36 -0300

    added drawing player sprite

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from constants import *
+import sys
 import pygame
+from constants import *
+from player import Player
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -18,7 +24,8 @@ def main():
 
         # Fill the screen with black
         screen.fill((0, 0, 0))
-
+        # Draw Player sprite
+        player.draw(screen)
         # Refresh the screen (must come last)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
